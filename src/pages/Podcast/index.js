b930df9a620c066fec72f640e3e504662e9abee6
joebegley15 <joebegley15@gmail.com>
2022-06-26 15:22:12 -0500

    Got the youtube api working

--- src/pages/Podcast/index.js
+++ src/pages/Podcast/index.js
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, { useEffect } from "react";
+import { get } from "axios";
 
-import './Podcast.scss';
+import "./Podcast.scss";
 
 const Podcast = () => {
+  useEffect(() => {
+    const channelId = "UUTyUgPNVujdkn4HVCRwFuiw";
+    const APIKey = "xxx";
+    const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=${channelId}&maxResults=5&key=${APIKey}`;
+    get(url).then(res => {
+      console.log(res);
+    });
+  }, []);
   return (
     <div id="podcast" className="background">
       <div className="top-padding"></div>
