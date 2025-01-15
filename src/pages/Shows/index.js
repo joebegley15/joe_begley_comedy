0a045087c464623d2b55985784cc54b44721de09
joebegley15 <joebegley15@gmail.com>
2025-01-15 17:17:51 -0600

    Added email list button

--- src/pages/Shows/index.js
+++ src/pages/Shows/index.js
@@ -7,17 +7,30 @@ import "./Shows.scss"
 const shows = require("./shows.json");
 
 const Shows = () => {
-    console.log(shows);
     return (
         <div id="shows" className="background">
             <div className="top-padding">
                 <FixedCenter>
                     {shows.map(show => {
                         return (
-                            <Show show={show}></Show>
-                        )
+                            <Show show={show} key={show.id}></Show>
+                        );
                     })}
                 </FixedCenter>
+            </div>
+            {/* Email List Section */}
+            <div className="email-list-container">
+                <p className="email-list-text">Don't see a show? I need emails to make a show in your city happen.</p>
+                <div className="button-container">
+                    <a
+                        href="https://docs.google.com/forms/d/e/1FAIpQLSe7qc131OjflHLorLBeXMMc7lx8G7EhZSZUwEzc-gFgTz4hUQ/viewform"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="form-button"
+                    >
+                        EMAIL LIST
+                    </a>
+                </div>
             </div>
         </div>
     );
